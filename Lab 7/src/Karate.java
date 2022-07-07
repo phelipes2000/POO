ad18f1141c64a5d0fe12f4be2bb457377e063c88
Leandro <llcostalonga@gmail.com>
2022-07-07 10:06:48 -0300

    Código exemplos lab 7 (updated)

--- Lab 7/src/Karate.java
+++ Lab 7/src/Karate.java
@@ -1,4 +1,7 @@
 
 public interface Karate extends ArteMarcial {
+	
+	public void magueri();
+	public void guedanBarai();
 
 }
--- Lab 7/src/UfcFighter.java
+++ Lab 7/src/UfcFighter.java
@@ -1,9 +1,21 @@
 
-public class UfcFighter extends Lutador implements Judo {
+public class UfcFighter extends Lutador implements Judo, Karate {
 
 	public UfcFighter(String nome) {
 		super(nome);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void magueri() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void guedanBarai() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public UfcFighter() {
